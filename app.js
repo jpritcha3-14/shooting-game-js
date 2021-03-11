3c6d9ed2f4a9f1b09473533c2b8dd9d6b4a01239
Jonathan Pritchard <jpritcha3.14@gmail.com>
2021-03-10 22:23:36 -0800

    handle shift for input, resize/recolor background

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const Sprite = {
-	x: 0,
-	y: 0,
-}
-
 const keys = new Set();
-["w", "a", "s", "d"].forEach(item => keys.add(item));
+["w", "a", "s", "d", "W", "A", "S", "D"].forEach(item => keys.add(item));
 
+// Global key state, only updated by key events
 var keyState = {
 	w: false,
 	a: false,
 	s: false,
 	d: false
 }
+
+const Sprite = {
+	x: 0,
+	y: 0,
+}
+
 
 spriteFactory = (image) => {
 	let sprite = Object.assign({img: new Image()}, Sprite);
@@ -23,19 +25,19 @@ var ship = Object.assign(spriteFactory('ship.png'), {speed: 5});
 
 init = () => {
 	updateState();
-	window.requestAnimationFrame(updateCanvas);
+	updateCanvas();
 }
 
 keyPressed = (event) => {
 	if (keys.has(event.key)) {
-		keyState[event.key] = true;
+		keyState[event.key.toLowerCase()] = true;
 		//console.log(keyState);
 	}
 }
 
 keyReleased = (event) => {
 	if (keys.has(event.key)) {
-		keyState[event.key] = false;
+		keyState[event.key.toLowerCase()] = false;
 		//console.log(keyState);
 	}
 }
@@ -52,13 +54,14 @@ updateState = () => {
 
 updateCanvas = () => {
 	window.requestAnimationFrame(updateCanvas);
-	
-	ctx = document.getElementById('test').getContext('2d');
-	ctx.clearRect(0, 0, 150, 150);
-	ctx.drawImage(ship.img, ship.x, ship.y)
+
+	let canvas = document.getElementById('test');
+	let ctx = canvas.getContext('2d');
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(ship.img, ship.x, ship.y);
 }
 
-
-window.onload = init
+window.onload = init;
 document.addEventListener("keydown", keyPressed);
 document.addEventListener("keyup", keyReleased);
